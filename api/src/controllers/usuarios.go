817facc38d30516c854d0e105bcbcc6d3c4dfc4b
Bruno Ricardo Correa <bruno.correa@sensedia.com>
2022-04-08 15:02:06 -0300

    First commit

---
+++ api/src/controllers/usuarios.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/models"
+	"api/src/repositories"
+	"api/src/responses"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		responses.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var usuario models.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = usuario.Preparar(); erro != nil {
+		responses.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		responses.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositories.NovoRepositorioDeUsuarios(db)
+	usuario.ID, erro = repositorio.Criar(usuario)
+	if erro != nil {
+		responses.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	responses.JSON(w, http.StatusCreated, usuario)
+}
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando todos os Usuários!"))
+}
+
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando um Usuário!"))
+}
+
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando Usuário!"))
+}
+
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deletando Usuário!"))
+}
---
+++ api/src/repositories/usuarios.go
@@ -0,0 +1,36 @@
+package repositories
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+type usuarios struct {
+	db *sql.DB
+}
+
+func NovoRepositorioDeUsuarios(db *sql.DB) *usuarios {
+	return &usuarios{db}
+}
+
+func (repositorio usuarios) Criar(usuario models.Usuario) (uint64, error) {
+	statement, erro := repositorio.db.Prepare(
+		"INSERT INTO usuarios (nome, nick, email, senha) VALUES(?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
+	}
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
+}
